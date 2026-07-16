a49fc0a85ccde684d8378d4c1cebc482f8609de9
Calvin <149830991+carinshark@users.noreply.github.com>
2026-07-16 15:48:22 -0700

    added a button class
    this lets you link elements to pre existing functions.

---
+++ button.py
@@ -0,0 +1,76 @@
+import pygame
+from appSettings import AppSettings
+
+class Button(pygame.Surface):
+    def __init__(self,icon:str,settings:AppSettings,
+                 function1,function2:function=None):
+        super().__init__((74,25))
+        self.settings=settings
+        self.function1:function=function1
+        self.function2:function=function2
+
+        self.bkg_img=pygame.image.load(icon)
+
+        self.pressed=False
+        self.is_active=False
+        
+        self.toggle=True if self.function2 else False
+
+        self.update()
+
+    def update(self):
+        self.blit(self.bkg_img)
+
+    def check_input(self,pos):
+        if self.pressed:
+            return
+        self.pressed=True
+        if not self.is_active:
+            self.function1()
+            if self.toggle:
+                self.is_active=True
+        else:
+            self.function2()
+            self.is_active=False
+            
+
+    def no_input(self):
+        self.pressed=False
+
+if __name__=="__main__":
+    pygame.init()
+
+    def testCommand():
+        print("pressed!")
+    def command2():
+        print("unclick")
+    
+    window = Button("artAssets/downloadImage.png",AppSettings(),testCommand,command2)
+    canvas = pygame.display.set_mode(window.size)
+    exit=False
+    active=False
+    
+    while not exit:
+        
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                exit=True
+            elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_ESCAPE:
+                    exit=True
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                if event.button==1:
+                    active=True
+                    window.check_input(event.pos)
+            elif event.type==pygame.MOUSEBUTTONUP:
+                if event.button==1:
+                    window.no_input()
+                    active=False
+
+            elif event.type==pygame.MOUSEMOTION:
+                if active:
+                    window.check_input(event.pos)
+
+        canvas.blit(window) 
+
+        pygame.display.update()
